0c2f6a28ff895ec1cfe074a9626b248a008f6965
chen nian <chen@nian.cn>
2026-03-26 18:19:43 +0800

    feat: test  hxr 2023.3.26

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @Description Test
+ * @Author nian
+ * @Date 2026-03-26
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("xixi");
+    }
+}
